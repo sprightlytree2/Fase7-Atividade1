0bdfe7de33c18746ac9ebea7c6dd26f070f7ca1b
sprightlytree2 <luccasdaccosta@gmail.com>
2023-11-17 21:52:17 -0300

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Olá, eu sou o Gulliver</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 5 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/profilePic2.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Gulliver Traveller
+      </a>
+      <div className={styles.menu}>
+        {/* <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        /> */}
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+          <li>
+            <a href="#experience">Formação</a>
+          </li>
+          <li>
+            <a href="#projects">Experiências</a>
+          </li>
+          <li>
+            <a href="#about">Hobbies</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
